54740fe4f92517142f8f233b1b49a65a414fe73f
938TGygg.github.io <jonas.resch.sh@gmail.com>
2023-07-15 17:27:04 +0200

    у

--- nicepage.js
+++ nicepage.js
@@ -47,6 +47,6 @@ window.addEventListener('load', function () {
     preloader.style.opacity = '0';
     setTimeout(function() {
     preloader.style.display = 'none';
-    }, 1000);
-    }, 500);
+    }, 10000);
+    }, 100);
     });
